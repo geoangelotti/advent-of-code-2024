420cbdd8f2cd36d15bfc2d1521b3a254e5f35d41
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-12-03 13:32:51 +0100

    same input on both tests

--- python/day_01/test.py
+++ python/day_01/test.py
@@ -1,7 +1,7 @@
 import pytest
 from lib import process_part_1
 
-INPUT_1 = '''3   4
+INPUT = '''3   4
 4   3
 2   5
 1   3
@@ -11,7 +11,7 @@ INPUT_1 = '''3   4
 
 
 @pytest.mark.parametrize("input,expected", [
-    (INPUT_1, 11)
+    (INPUT, 11)
 ])
 def test_part_1(input: str, expected: int):
     assert process_part_1(input) == expected
