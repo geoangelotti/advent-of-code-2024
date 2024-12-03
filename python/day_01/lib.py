501db9921ef0d83c1756fab26a39fbe38129cf49
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-12-03 13:38:14 +0100

    solution

--- python/day_01/lib.py
+++ python/day_01/lib.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def process_part_1(input: str) -> int:
     l1 = []
     l2 = []
@@ -12,4 +15,12 @@ def process_part_1(input: str) -> int:
 
 
 def process_part_2(input: str) -> int:
-    pass
+    l1 = []
+    l2 = []
+    for line in input.split("\n"):
+        numbers = line.split("   ")
+        if numbers.__len__() == 2:
+            l1.append(int(numbers[0]))
+            l2.append(int(numbers[1]))
+    map = Counter(l2)
+    return sum([i * map.get(i, 0) for i in l1])
