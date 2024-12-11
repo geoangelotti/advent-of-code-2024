2e150295b4bb44216912203fcea92334da6d8413
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-12-11 18:10:21 +0100

    input for part 1

--- go/day03/lib_test.go
+++ go/day03/lib_test.go
@@ -5,12 +5,12 @@ import (
 	"testing"
 )
 
-const INPUT = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+const INPUT1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 const INPUT2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 func TestProcessPart1(t *testing.T) {
 	expected := 161
-	answer := day03.ProcessPart1(INPUT)
+	answer := day03.ProcessPart1(INPUT1)
 	if answer != expected {
 		t.Errorf("Expected %d got %d", expected, answer)
 	}
