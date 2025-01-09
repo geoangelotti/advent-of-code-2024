a92f27489e6f28862f88c1791d825d5a2997cd6f
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-01-09 22:10:16 +0100

    parse grid

--- go/day04/lib.go
+++ go/day04/lib.go
@@ -1,5 +1,24 @@
 package day04
 
+import (
+	"fmt"
+	"strings"
+)
+
+func parseGrid(input string) [][]rune {
+	grid := [][]rune{}
+	for _, line := range strings.Split(input, "\n") {
+		row := []rune{}
+		for _, char := range line {
+			row = append(row, char)
+		}
+		grid = append(grid, row)
+	}
+	return grid
+}
+
 func ProcessPart1(input string) int {
+	grid := parseGrid(input)
+	fmt.Println(grid)
 	return 0
 }
