ea138e78181886a24a9a9842ec224bd1a5b52bb4
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-02-04 23:01:53 +0100

    simplify input

--- go/day05/lib.go
+++ go/day05/lib.go
@@ -2,47 +2,30 @@ package day05
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 )
 
-func parseInput(input string) ([][]int, [][]int) {
+func parseInput(input string) ([]string, [][]string) {
 	part1 := strings.Split(input, "\n\n")[0]
 	part2 := strings.Split(input, "\n\n")[1]
-	rules := [][]int{}
-	pages := [][]int{}
-	for _, line := range strings.Split(part1, "\n") {
-		fragments := strings.Split(line, "|")
-		page1, _ := strconv.Atoi(fragments[0])
-		page2, _ := strconv.Atoi(fragments[1])
-		rule := []int{page1, page2}
-		rules = append(rules, rule)
-	}
+	rules := strings.Split(part1, "\n")
+	pages := [][]string{}
 	for _, line := range strings.Split(part2, "\n") {
 		fragments := strings.Split(line, ",")
-		page := []int{}
-		for _, fragment := range fragments {
-			pageNum, _ := strconv.Atoi(fragment)
-			page = append(page, pageNum)
-		}
-		pages = append(pages, page)
+		pages = append(pages, fragments)
 	}
 	return rules, pages
 }
 
 func ProcessPart1(input string) int {
 	rules, pages := parseInput(input)
-	orderingRules := map[int][]int{}
+	orderingRules := map[string]bool{}
 	for _, rule := range rules {
-		if page, ok := orderingRules[rule[0]]; ok {
-			fmt.Println(page)
-			page = append(page, rule[1])
-			orderingRules[rule[0]] = page
-		} else {
-			orderingRules[rule[0]] = []int{rule[1]}
-		}
+		orderingRules[rule] = true
 	}
 	fmt.Println(rules, pages)
-	fmt.Println(orderingRules)
+	for k, v := range orderingRules {
+		fmt.Println(k, v)
+	}
 	return 0
 }
