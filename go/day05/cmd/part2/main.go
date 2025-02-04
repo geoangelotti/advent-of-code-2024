d65c72db0f4c4fa2c61aa7e281689cfbca4a4162
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-02-04 23:50:38 +0100

    executable

---
+++ go/day05/cmd/part2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"day05"
+	"fmt"
+	"os"
+)
+
+func main() {
+	content, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(os.Getwd())
+	}
+	fmt.Println(day05.ProcessPart2(string(content)))
+}
