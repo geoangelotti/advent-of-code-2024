ee8b37a808dfe6adab7e56caa7825de22ce832de
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-02-04 22:21:31 +0100

    parse input

--- go/day05/lib.go
+++ go/day05/lib.go
@@ -1,6 +1,37 @@
 package day05
 
-func ProcessPart1(input string) int {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+func parseInput(input string) ([][]int, [][]int) {
+	part1 := strings.Split(input, "\n\n")[0]
+	part2 := strings.Split(input, "\n\n")[1]
+	rules := [][]int{}
+	pages := [][]int{}
+	for _, line := range strings.Split(part1, "\n") {
+		fragments := strings.Split(line, "|")
+		page1, _ := strconv.Atoi(fragments[0])
+		page2, _ := strconv.Atoi(fragments[1])
+		rule := []int{page1, page2}
+		rules = append(rules, rule)
+	}
+	for _, line := range strings.Split(part2, "\n") {
+		fragments := strings.Split(line, ",")
+		page := []int{}
+		for _, fragment := range fragments {
+			pageNum, _ := strconv.Atoi(fragment)
+			page = append(page, pageNum)
+		}
+		pages = append(pages, page)
+	}
+	return rules, pages
+}
+
+func ProcessPart1(input string) int {
+	rules, pages := parseInput(input)
+	fmt.Println(rules, pages)
 	return 0
 }
